60f320949ab8579aa4334cbfb69628f40d03805b
kaltompe123 <kaltompe123@gmail.com>
2025-07-19 17:23:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+def greet(name):
+    """
+    Function to greet a user by name.
+    Args:
+        name (str): The name of the user.
+    Returns:
+        str: Greeting message.
+    """
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
